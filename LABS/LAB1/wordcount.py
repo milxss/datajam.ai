ec824f071078f44643893a71eb4f6574fed87f91
milxss <42537931+milxss@users.noreply.github.com>
2020-12-12 00:53:53 +0100

    Update wordcount.py

--- LABS/LAB1/wordcount.py
+++ LABS/LAB1/wordcount.py
@@ -1,1 +1,10 @@
+def number_words(text):
+    print('String-', text)
+    no_of_words = 1
+    for ch in text:
+        if (ch == ' ' or ch == '\t' or ch == '\n'):
+            no_of_words += 1
+    print('Total number of words in String', no_of_words)
 
+s = 'This Python program counts\tnumber of words in a String.'
+number_words(s)
